9ecd229d197ff780ba750fc3639c74ea7eb16697
eeshaan <31187740+eeshaan@users.noreply.github.com>
2018-04-08 12:50:24 -0500

    Fix About page navbar on scroll

---
+++ src/js/about-navbar.js
@@ -0,0 +1,17 @@
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('inverse');
+  } else {
+    $('nav').removeClass('inverse');
+  }
+  if ($(document).scrollTop() > 50) {
+    $('.nav-link').addClass('text-white');
+  } else {
+    $('.nav-link').removeClass('text-white');
+  }
+  if ($(document).scrollTop() > 50) {
+    $('.navbar-brand').addClass('text-white');
+  } else {
+    $('.navbar-brand').removeClass('text-white');
+  }
+});  
