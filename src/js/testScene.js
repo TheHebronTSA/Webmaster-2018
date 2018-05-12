1a0a1fafdbe27528dde26ade24a9f1fbb7dfd95f
softwaeres <33181927+softwaeres@users.noreply.github.com>
2018-05-12 13:11:15 -0500

    updated testScene

--- src/js/testScene.js
+++ src/js/testScene.js
@@ -5,13 +5,6 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
 
 var loader = new THREE.ColladaLoader();
 loader.load('./src/3dModels/model.dae', function (result) {
