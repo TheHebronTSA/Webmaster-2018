9b6288ef63d8ae12c4f007ef3570f63cdb21318b
softwaeres <33181927+softwaeres@users.noreply.github.com>
2018-05-12 12:49:36 -0500

    format changes

--- src/js/testScene.js
+++ src/js/testScene.js
@@ -1,29 +1,26 @@
 var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			var cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-			camera.position.z = 5;
+camera.position.z = 5;
 
-			var animate = function () {
-				requestAnimationFrame( animate );
 
 var loader = new THREE.ColladaLoader();
-loader.load('/src/3dModels/model.dae',function(result)
-{
+loader.load('./src/3dModels/model.dae', function (result) {
     scene.add(result.scene);
 });
 
 
 
-//var light = new THREE.PointLight(0xFFFF00);
-//light.position.set(10, 0, 25);
-//scene.add(light);
-renderer.render(scene,camera);
+var light = new THREE.PointLight(0xFFFF00);
+light.position.set(10, 0, 25);
+scene.add(light);
+renderer.render(scene, camera);
