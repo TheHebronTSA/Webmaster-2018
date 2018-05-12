7ad8176b86d6091a1e403643339e0423d8d28e3c
softwaeres <33181927+softwaeres@users.noreply.github.com>
2018-05-12 12:39:47 -0500

    ERROR WITH three.js
    ERROR WITH three.js

--- src/js/testScene.js
+++ src/js/testScene.js
@@ -1,30 +1,24 @@
 var scene = new THREE.Scene();
+			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+			var renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
 
-camera.position.z = 100;
+			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			var cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth,window.innerHeight);
-renderer.setClearColor(0xfff6e6);
-document.body.appendChild(renderer.domElement);
+			camera.position.z = 5;
 
+			var animate = function () {
+				requestAnimationFrame( animate );
 
-var loader = new THREE.ColladaLoader();
-loader.load('/src/3dModels/model.dae',function(collada)
-{
-    var dae = collada.scene;
-    var skin = collada.skins[0];
+				cube.rotation.x += 0.1;
+				cube.rotation.y += 0.1;
 
-    dae.position.set(0,0,0);
-    dae.scale.set(1.5,1.5,1.5);
-    scene.add(dae);
-});
+				renderer.render(scene, camera);
+			};
 
-
-
-
-var light = new THREE.PointLight(0xFFFF00);
-light.position.set(10, 0, 25);
-scene.add(light);
-renderer.render(scene,camera);
+			animate();
