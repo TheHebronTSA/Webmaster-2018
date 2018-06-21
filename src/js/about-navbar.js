91a3b7fe4e70ea592f57378a88697d7f880428bb
eeshaan <31187740+eeshaan@users.noreply.github.com>
2018-06-20 22:52:05 -0500

    Clean-up about.html and update about page responsiveness
    Remove AOS

--- src/js/about-navbar.js
+++ src/js/about-navbar.js
@@ -1,27 +1,15 @@
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
     $('nav').addClass('inverse');
-  } else {
-    $('nav').removeClass('inverse');
-  }
-  if ($(document).scrollTop() > 50) {
     $('.nav-link').addClass('text-white');
-  } else {
-    $('.nav-link').removeClass('text-white');
-  }
-  if ($(document).scrollTop() > 50) {
     $('.navbar-brand').addClass('text-white');
-  } else {
-    $('.navbar-brand').removeClass('text-white');
-  }
-  if ($(document).scrollTop() > 50) {
     $('.navbar-toggler').addClass('custom-toggler');
-  } else {
-    $('.navbar-toggler').removeClass('custom-toggler');
-  }
-  if ($(document).scrollTop() > 50) {
     $('.nav-link').addClass('hvr-underline-from-center-white');
   } else {
+    $('nav').removeClass('inverse');
+    $('.nav-link').removeClass('text-white');
+    $('.navbar-brand').removeClass('text-white');
+    $('.navbar-toggler').removeClass('custom-toggler');
     $('.nav-link').removeClass('hvr-underline-from-center-white');
-  }
-});  
+  } 
+})
